b8625d6debba70b8973f4f0d15c7c9d7546d92d6
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2021-01-03 18:44:01 -0500

    updated and reviewed find_judge graph

--- src/demos/demo2.py
+++ src/demos/demo2.py
@@ -2,9 +2,11 @@
 In a town, there are `N` people labelled from `1` to `N`.  There is a rumor
 that one of these people is secretly the town judge.
 If the town judge exists, then:
+
 1. The town judge trusts nobody.
 2. Everybody (except for the town judge) trusts the town judge.
 3. There is exactly one person that satisfies properties 1 and 2.
+
 You are given `trust`, an array of pairs `trust[i] = [a, b]` representing that
 the person labelled a trusts the person labelled `b`.
 If the town judge exists and can be identified, return the label of the town
@@ -51,28 +53,43 @@ def find_judge(N, trust):
     int
     """
     # Your code here
-
     # base case
     if len(trust) <  N - 1:
         return -1
 
+    # indegree ---> num of directed edges into a vertex == N-1
+    # outdegree ---> num of directed edges from a vertex == 0
+
+    # N+1 so not to go out of bounds, accounts for 0
+    # create an indegree list
     indegree = [0] * (N + 1)
+    # create an outdegree list
     outdegree = [0] * (N + 1)
 
     for a, b in trust:
         outdegree[a] += 1
         indegree[b] += 1
 
+    print(outdegree,indegree)
+    
+    # iterate check indegree and outdegree
     for i in range(1, N + 1):
+        # check both conditions
         if indegree[i] == N - 1 and outdegree[i] == 0:
+            # return index i
             return i
     
+    # otherwise return -1 False
     return -1
 
 # Input: 
 N = 4
 trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
 # Output: 3
+print(find_judge(N, trust))
 
 
+N = 3
+trust = [[1,2],[2,3]]
+#Output: -1
 print(find_judge(N, trust))
